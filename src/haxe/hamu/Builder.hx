2ce72b32dccfb2a98c6ce6a5fea14d040ae2c685
Yang Bo <atryyang@thoughtworks.com>
2016-02-10 23:57:14 +0800

    Optimize performance for lazyDefineMacroClass and lazyDefineClass

--- src/haxe/hamu/Builder.hx
+++ src/haxe/hamu/Builder.hx
@@ -9,6 +9,43 @@ typedef FieldsGenerator<Argument> = {
   function fields(argument:Argument, buildingModule:String, buildingClassName:String):Array<Field>;
 }
 
+#if macro
+
+private class LazyTypeFactory {
+
+  var lazyClasses:Null<StringMap<TypeDefinition>>;
+
+  public function new() {}
+
+  public function defineType(moduleName:String, typeBuilder:TypeDefinition):Void {
+    if (lazyClasses == null) {
+      lazyClasses = new StringMap<TypeDefinition>();
+      Context.onAfterGenerate(function() {
+        lazyClasses = null;
+      });
+      Context.onTypeNotFound(function(moduleName) return {
+        switch lazyClasses.get(moduleName) {
+          case null: null;
+          case builder:
+            builder;
+        }
+      });
+    }
+    if (lazyClasses.exists(moduleName)) {
+      trace('Duplicated lazy module: $moduleName');
+    } else {
+      lazyClasses.set(moduleName, typeBuilder);
+    }
+  }
+
+  public static var TARGET_FACTORY(default, never) = new LazyTypeFactory();
+
+  public static var MACRO_FACTORY(default, never) = new LazyTypeFactory();
+
+}
+
+#end
+
 class Builder<Argument> {
 
   var fieldsGenerator: FieldsGenerator<Argument>;
@@ -19,17 +56,10 @@ class Builder<Argument> {
 
   #if macro
 
-
-  /**
-   * Define a class `buildingModule`.`buildingClassName` with metadata `meta`.
-   *
-   * If calling this method to generate same class more than once,
-   * the second call will be ignored.
-   */
-  public function lazyDefineClass(argument:Argument, buildingModule:String, ?buildingClassName:String, ?meta:Metadata):Void {
+  function internalLazyDefineClass(typeFactory:LazyTypeFactory, argument:Argument, buildingModule:String, ?buildingClassName:String, ?meta:Metadata):Void {
     var parserPackage = buildingModule.split(".");
     var moduleName = parserPackage.pop();
-    var parserDefinition = {
+    typeFactory.defineType(buildingModule, {
       pack: parserPackage,
       name: buildingClassName == null ? moduleName : buildingClassName,
       pos: PositionTools.here(),
@@ -38,14 +68,17 @@ class Builder<Argument> {
       kind: TDClass(null, [], false),
       isExtern: false,
       fields: fieldsGenerator.fields(argument, buildingModule, buildingClassName == null ? moduleName : buildingClassName)
-    };
-    Context.onTypeNotFound(function(name) return {
-      if (name == buildingModule) {
-        parserDefinition;
-      } else {
-        null;
-      }
     });
+  }
+
+  /**
+   * Define a class `buildingModule`.`buildingClassName` with metadata `meta`.
+   *
+   * If calling this method to generate same module more than once,
+   * the second call will be ignored.
+   */
+  public function lazyDefineClass(argument:Argument, buildingModule:String, ?buildingClassName:String, ?meta:Metadata):Void {
+    internalLazyDefineClass(LazyTypeFactory.TARGET_FACTORY, argument, buildingModule, buildingClassName, meta);
   }
 
   /**
@@ -70,14 +103,20 @@ class Builder<Argument> {
     Context.defineModule(buildingModule, [ parserDefinition ]);
   }
 
+  /**
+   * Define a class `buildingModule`.`buildingClassName` with metadata `meta` in macro context.
+   *
+   * If calling this method to generate same module more than once,
+   * the second call will be ignored.
+   */
   public function lazyDefineMacroClass(argument:Argument, buildingModule:String, ?buildingClassName:String, ?meta:Metadata):Void {
     hamu.ExprEvaluator.runInMacroContext(function () {
-      lazyDefineClass(argument, buildingModule, buildingClassName, meta);
+      internalLazyDefineClass(LazyTypeFactory.MACRO_FACTORY, argument, buildingModule, buildingClassName, meta);
     });
   }
 
   /**
-   * Define a class `buildingModule`.`buildingClassName` with metadata `meta` in macro scope.
+   * Define a class `buildingModule`.`buildingClassName` with metadata `meta` in macro context.
    *
    * If calling this method to generate same class more than once,
    * a compilation error will be raised.
